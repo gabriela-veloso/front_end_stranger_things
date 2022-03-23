a20edc60cc416403656fbae08163b0826a4002d5
gabriela-veloso <gabrielaveloso.engmateriais@gmail.com>
2022-03-23 10:12:34 -0300

    Repositorio Stranger Things Front End

---
+++ src/services/charactersAPI.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+require('dotenv').config();
+
+const DEFAULT_TIMEOUT = 30000;
+class CharactersService {
+  constructor({ url = process.env.REACT_APP_HAWKINS_URL, timeout = DEFAULT_TIMEOUT }) {
+    this.http = axios.create({
+      baseURL: url,
+      timeout,
+    });
+  }
+
+  async getCharacters(name, page, size) {
+    const params = {
+      page,
+      size,
+      name,
+    };
+
+    return this.http.get('/', { params });
+  }
+}
+
+export default CharactersService;
